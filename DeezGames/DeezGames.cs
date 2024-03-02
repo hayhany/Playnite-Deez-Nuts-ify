5f25f1a5be19bb47970811607e94793ba18f2b50
hayhany <hayhany1@gmail.com>
2024-03-02 12:26:57 +0200

    Add files via upload

---
+++ DeezGames/DeezGames.cs
@@ -0,0 +1,99 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DeezGames
+{
+    public class DeezGames : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private DeezGamesSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("32c32b50-c9a9-44cd-bd5b-e7869d065ee7");
+
+        public DeezGames(IPlayniteAPI api) : base(api)
+        {
+            settings = new DeezGamesSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMenuItem> GetGameMenuItems(GetGameMenuItemsArgs menuItemArgs)
+        {
+            yield return new GameMenuItem
+            {
+                Description = "Deez nuts-ify",
+                Action = (args) =>
+                {
+                    foreach (Game game in args.Games)
+                    {
+                        game.Name = StringUtils.DeezNutsify(game.Name);
+                        PlayniteApi.Database.Games.Update(game);
+                    }
+                    // use args.Games to get list of games attached to the menu source
+
+                }
+            };
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new DeezGamesSettingsView();
+        }
+    }
+}
---
+++ DeezGames/StringUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DeezGames
+{
+    /* RULES:
+     * 
+     * 1. if has "of" add deez nuts after (removes all text after) ✅
+     * 2. if has (the) but without (of) before it, remove all after, add deez nuts and add last word ✅
+     * 3. if has (') add deez nuts before it
+     * 4. if has (a) remove "a", add deez nuts and remove everything after
+     * 5. if has (Edition) replace word before
+     * 6. if not meeting any, add "of Deez Nuts" after
+     */
+
+    public static class StringUtils
+    {
+        public static string DeezNutsify(string inputString)
+        {
+            string[] split = inputString.Split(' ');
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                string lowered = split[i].ToLower();
+
+                if (lowered.Equals("of"))
+                    return GetUntilEndAndAddDeez(split, i + 1, false);
+
+                else if (lowered.Equals("the") && GetFollowsSecondRule(split, i))
+                    return GetUntilEndAndAddDeez(split, i + 1, true);
+
+                else if (lowered.Equals("a"))
+                    return GetUntilEndAndAddDeez(split, i, true);
+
+                else if (lowered.Contains("'"))
+                    return inputString.Replace(split[i], "Deez Nuts'");
+
+                else if (lowered.Equals("edition"))
+                    return inputString.Replace(split[i - 1], "Deez Nuts");
+            }
+
+            return inputString += " of Deez Nuts";
+        }
+
+        public static bool GetFollowsSecondRule(string[] inputSplit, int wordIndex)
+        {
+            if (wordIndex == 0)
+                return true;
+
+            return inputSplit[wordIndex - 1].ToLower().Equals("of");
+        }
+
+        public static string GetUntilEndAndAddDeez(string[] inputSplit, int endIndex, bool addLastWord)
+        {
+            string output = string.Join(" ", inputSplit, 0, endIndex);
+
+            output += " Deez Nuts ";
+
+            if (addLastWord)
+            {
+                string last = inputSplit[inputSplit.Length - 1];
+                output += last;
+            }
+
+            return output;
+        }
+    }
+}
